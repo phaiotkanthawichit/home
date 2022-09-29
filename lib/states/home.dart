348eede00a44aa949e0a5635f7e123dbd6948d07
a4sme <a4sme.com@gmail.com>
2022-09-29 18:32:31 +0700

    29-19-2022

--- lib/states/home.dart
+++ lib/states/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,7 +12,11 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('This is home'),
+      body: ScreenTypeLayout(
+        desktop: Text('This is Home type Desktop'),
+        tablet: Text('This is Home type Tablet'),
+        mobile: Text('This is Home type Mobile'),
+      ),
     );
   }
 }
